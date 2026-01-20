fe53f77e091f51322ad1fac4e74cc62e8c5231e0
myname1111 <animationstudiocp@gmail.com>
2026-01-20 22:33:38 +0700

    Change to winit

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,28 @@ mod simulation;
 use std::time::SystemTime;
 
 use simulation::{Particle, Simulation};
+use winit::{application::ApplicationHandler, event_loop::EventLoop};
 
 use crate::{renderer::SimulationRenderer, simulation::BASE_PARTICLE_RADIUS};
 
-fn main() {
+struct App;
+
+impl ApplicationHandler<()> for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        todo!()
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        todo!()
+    }
+}
+
+fn main() -> anyhow::Result<()> {
     env_logger::init();
     let mut simulation = Simulation::new([60, 40]);
     for x in 0..10 {
@@ -33,4 +51,8 @@ fn main() {
     //     pos: [130.0, 800.0],
     //     velocity: [100.0, 0.0],
     // });
+
+    let event_loop = EventLoop::with_user_event().build()?;
+
+    Ok(())
 }
