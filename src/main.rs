509e8d3af598027911a5eab2f209ad6aeaea208a
myname1111 <animationstudiocp@gmail.com>
2026-01-20 22:16:56 +0700

    Remove piston

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,12 @@ mod simulation;
 
 use std::time::SystemTime;
 
-use piston::{Button, ButtonArgs, Event, Input, Key, WindowSettings};
-use piston_window::{EventLoop, Graphics, PistonWindow};
 use simulation::{Particle, Simulation};
 
 use crate::{renderer::SimulationRenderer, simulation::BASE_PARTICLE_RADIUS};
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Fluid simulation", [1000, 1000])
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
+    env_logger::init();
     let mut simulation = Simulation::new([60, 40]);
     for x in 0..10 {
         for y in 0..10 {
@@ -39,39 +33,4 @@ fn main() {
     //     pos: [130.0, 800.0],
     //     velocity: [100.0, 0.0],
     // });
-
-    window.set_lazy(false);
-    let mut prev_frame = SystemTime::now();
-    let mut frame_idx = 0;
-    let mut total_time = 0.0;
-    window.events.max_fps(60);
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |ctx, graphics_buffer, _device| {
-            let dt = SystemTime::now()
-                .duration_since(prev_frame)
-                .expect("Time may have gone backwatds");
-            total_time += dt.as_secs_f64();
-            prev_frame = SystemTime::now();
-            // simulation.simulate(dt.as_secs_f64());
-            graphics_buffer.clear_color([1.0, 1.0, 1.0, 1.0]);
-
-            // let mut simulation_renderer = SimulationRenderer::new(&ctx, graphics_buffer);
-            // simulation_renderer.render(&simulation);
-            frame_idx += 1;
-            // dbg!(dt);
-        });
-
-        if let Event::Input(
-            Input::Button(ButtonArgs {
-                button: Button::Keyboard(Key::D),
-                ..
-            }),
-            _,
-        ) = event
-        {
-            simulation.debug();
-        }
-    }
-
-    println!("Average frame rate {}", frame_idx as f64 / total_time)
 }
